29d4b677009cd3b62657ab616fa5c78c4b7e7838
Tadas <tadasgailevicius@yahoo.com>
2021-06-17 14:29:25 +0300

    Update AndroidManifest.xml Manifest

--- app/src/main/java/com/tedm/gnewsapp/ui/MainActivity.kt
+++ app/src/main/java/com/tedm/gnewsapp/ui/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.tedm.gnewsapp
+package com.tedm.gnewsapp.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.tedm.gnewsapp.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
