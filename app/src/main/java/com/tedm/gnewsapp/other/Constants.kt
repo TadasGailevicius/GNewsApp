16436d011f2aa44c724cc7a4c6f447c2a573a415
Tadas <tadasgailevicius@yahoo.com>
2021-06-17 15:33:15 +0300

    Add Constants.kt Constants

---
+++ app/src/main/java/com/tedm/gnewsapp/other/Constants.kt
@@ -0,0 +1,10 @@
+package com.tedm.gnewsapp.other
+
+class Constants {
+    companion object {
+        const val API_KEY = "4dfec9ad434c42df4c67b94be70d6f41"
+        const val BASE_URL = "https://gnews.io"
+        const val QUERY_PAGE_SIZE = 20
+
+    }
+}
